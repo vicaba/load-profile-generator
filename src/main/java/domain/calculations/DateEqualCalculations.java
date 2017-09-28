72aeec909858385e50349a3ea0188d5784289112
Albert <atriast@gmail.com>
2017-09-28 11:06:14 +0200

    Added Javadoc comments to the classes inside the calculations package. Also used to make IntelliJ to remember not to reformat the code so it preserves the Google Java Format specifications.

--- src/main/java/domain/calculations/DateEqualCalculations.java
+++ src/main/java/domain/calculations/DateEqualCalculations.java
@@ -3,27 +3,56 @@ package domain.calculations;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Class responsible of giving a valid date field depending on the input configurations that are
+ * passed.
+ *
+ * @version 1.0
+ * @author Albert Trias
+ * @since 28/09/2017
+ */
 public class DateEqualCalculations implements EqualCalculations<String> {
-    private String startingDate;
-    private DateTimeFormatter dateTimeFormatter;
-    private int timeIncrement;
-    private int cycle;
+  /** Defines the format in which the date will appear in the output data. */
+  private static final String DATE_FORMAT = "dd-MM-yyyy HH:mm";
 
-    private static final String DATE_FORMAT = "dd-MM-yyyy HH:mm";
+  /** Starting date. Date values will never go below this one. */
+  private String startingDate;
+  /** How much time in seconds there is between created dates */
+  private int timeIncrement;
+  /** The current cycle. Used to choose the next X date after the starting date */
+  private int cycle;
 
-    public DateEqualCalculations(String startingDate, int timeIncrement, int cycle) {
-        this.dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
-        this.startingDate = startingDate;
-        this.timeIncrement = timeIncrement;
-        this.cycle = cycle;
-    }
+  private DateTimeFormatter dateTimeFormatter;
 
-    @Override
-    public String calculate() {
-        LocalDateTime localDateTime =
-                LocalDateTime.parse(this.startingDate, this.dateTimeFormatter)
-                        .plusSeconds(this.timeIncrement * this.cycle);
+  /**
+   * Constructor.
+   *
+   * @param startingDate String with the initial date. Dates below the initial date will never be
+   *     generated.
+   * @param timeIncrement Integer with the amount of time that will pass every time a new date is
+   *     generated.
+   * @param cycle Integer with the current data cycle we are in at the moment.
+   */
+  public DateEqualCalculations(String startingDate, int timeIncrement, int cycle) {
+    this.dateTimeFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    this.startingDate = startingDate;
+    this.timeIncrement = timeIncrement;
+    this.cycle = cycle;
+  }
 
-        return localDateTime.format(dateTimeFormatter);
-    }
+  /**
+   * Function responsible of giving a valid date according to the configuration passed in the
+   * constructor. The next date will be based according to the formula startingDate +
+   * timeIncrement*cycle.
+   *
+   * @return A valid date according to the formula mentioned above.
+   */
+  @Override
+  public String calculate() {
+    LocalDateTime localDateTime =
+        LocalDateTime.parse(this.startingDate, this.dateTimeFormatter)
+            .plusSeconds(this.timeIncrement * this.cycle);
+
+    return localDateTime.format(dateTimeFormatter);
+  }
 }
--- src/main/java/domain/calculations/NumberEqualCalculations.java
+++ src/main/java/domain/calculations/NumberEqualCalculations.java
@@ -5,19 +5,45 @@ import domain.options.NumberRange;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * Class responsible of giving a valid number field depending on the input configurations that are
+ * passed.
+ *
+ * @version 1.0
+ * @author Albert Trias
+ * @since 28/09/2017
+ */
 public class NumberEqualCalculations implements EqualCalculations<Float> {
-    private ArrayList<NumberRange> numberRanges;
+  /** List of ranges, one of them will be chosen at random. */
+  private ArrayList<NumberRange> numberRanges;
 
-    public NumberEqualCalculations(ArrayList<NumberRange> numberRanges) {
-        this.numberRanges = numberRanges;
-    }
+  /**
+   * Constructor.
+   *
+   * @param numberRanges ArrayList of NumberRange with the list of ranges available for this field.
+   */
+  public NumberEqualCalculations(ArrayList<NumberRange> numberRanges) {
+    this.numberRanges = numberRanges;
+  }
 
-    @Override
-    public Float calculate() {
-        NumberRange numberRange =
-                this.numberRanges.get(ThreadLocalRandom.current().nextInt(0, this.numberRanges.size()));
+  /**
+   * Function responsible of giving a valid number according to the configuration passed in the
+   * constructor. The number will be chosen by first choosing a random range from the list, and
+   * after that a number will be chosen between the given range
+   *
+   * <p>The formula used to obtain the number between the chosen range is randomFloat * (max-min) +
+   * min, including min and excluding max value.
+   *
+   * @return Float with the random number obtaining after choosing a random range and number.
+   */
+  @Override
+  public Float calculate() {
+    // First we obtain the random range from the list of ranges
+    NumberRange numberRange =
+        this.numberRanges.get(ThreadLocalRandom.current().nextInt(0, this.numberRanges.size()));
 
-        float fMin = numberRange.getMin();
-        return ThreadLocalRandom.current().nextFloat() * (numberRange.getMax() - fMin) + fMin;
-    }
+    // Then we obtain a random number between inclusive min and exclusive max of that range.
+    float fMin = numberRange.getMin();
+    return ThreadLocalRandom.current().nextFloat() * (numberRange.getMax() - fMin) + fMin;
+  }
 }
