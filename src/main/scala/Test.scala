f1b0bc4c85df36d53c1710bf5e890ba0a79fd2e6
Albert <atriast@gmail.com>
2017-10-18 14:03:08 +0200

    Testing merge.

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,6 +1,8 @@
+import java.time.LocalDateTime
+
 import akka.{Done, NotUsed}
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream._
 import akka.stream.scaladsl._
 import domain.in.config.ConfigHolder
 import domain.in.field.InputField
@@ -16,8 +18,7 @@ import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.util.Success
 
 object Test extends App {
-
-  implicit def sourceValueTToSource[V](st: SourceValueT[V, _]): Source[Value[V], NotUsed] = Source.fromGraph(st)
+  //implicit def sourceValueTToSource[V](st: SourceValueT[V, _]): Source[Value[V], NotUsed] = Source.fromGraph(st)
 
   implicit val system: ActorSystem = ActorSystem("QuickStart")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
@@ -34,9 +35,9 @@ object Test extends App {
     new NumberEqualCalculations(inputFieldNumber.getOptions.getRanges)
   )
   val sourceGraphNumber = new SourceValueNumber(dataGeneratorNumber)
-  val doneNumber: Future[Done] = sourceGraphNumber
+/*  val doneNumber: Future[Done] = sourceGraphNumber
     .take(10)
-    .runWith(Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString)))
+    .runWith(Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString)))*/
 
   val inputFieldString =
     configGen.getField(0).asInstanceOf[InputField[OptionsString]]
@@ -45,9 +46,9 @@ object Test extends App {
     new StringEqualCalculations(inputFieldString.getOptions.getAcceptedStrings)
   )
   val sourceGraphString = new SourceValueString(dataGeneratorString)
-  val doneString: Future[Done] = sourceGraphString
+/*  val doneString: Future[Done] = sourceGraphString
     .take(10)
-    .runWith(Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString)))
+    .runWith(Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString)))*/
 
   val inputFieldDate =
     configGen.getField(2).asInstanceOf[InputField[OptionsDate]]
@@ -59,12 +60,29 @@ object Test extends App {
     )
   )
   val sourceGraphDate = new SourceValueDate(dataGeneratorDate)
-  val doneDate: Future[Done] = sourceGraphDate
+/*  val doneDate: Future[Done] = sourceGraphDate
     .take(10)
-    .runWith(Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString)))
+    .runWith(Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString)))*/
 
+
+  val merged = Source.combine(
+    Source.fromGraph(sourceGraphString),
+    Source.fromGraph(sourceGraphNumber),
+    Source.fromGraph(sourceGraphDate)
+  )(Merge(_))
+  
+  //TODO The combine method merges all sources into one.
+  //TODO If for example we have 3 columns, it will generate an entire data each 3 iterations (test below generates 10 data of 3 columns).
+  //TODO The plan would be to grab the 3 columns, put them together in one single List, and pass them to a Sink
+  //TODO This sink will do whatever job we need it to do with the merged value.
+  //TODO Now to figure out how to do it with any number of columns...
+  val mergedResult: Future[Done] = merged.take(30).runWith(
+    Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString))
+  )
+
+  /*
   Future.sequence(Seq(doneNumber, doneString, doneDate)).andThen {
     case _ => system.terminate()
   }
-
+  */
 }
