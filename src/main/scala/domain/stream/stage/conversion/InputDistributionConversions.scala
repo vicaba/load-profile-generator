e6fa6e456801b5716f255403c8cb026e3d87ff1c
Albert <atriast@gmail.com>
2017-11-07 16:14:18 +0100

    The changes done to InputDistributionConversions in previous merge stopped the node from applying a distribution. I reverted the file back to the previous version and created a separate class that contains the new version that doesn't work.

--- src/main/scala/domain/stream/stage/conversion/InputDistributionConversions.scala
+++ src/main/scala/domain/stream/stage/conversion/InputDistributionConversions.scala
@@ -4,32 +4,42 @@ import akka.NotUsed
 import akka.stream.scaladsl.Flow
 import domain.in.distribution.InputDistribution
 import domain.in.field.InputField
-import domain.stream.stage.flow.distribution.{DistributionFlowDate, DistributionFlowNumber, DistributionFlowString}
-
+import domain.in.field.options.{OptionsDate, OptionsNumber, OptionsString}
+import domain.stream.stage.flow.distribution.DistributionFlowDate
+import domain.transform.calculations.distribution.DateDistributionCalculations
+import domain.transform.calculations.equal.{NumberEqualCalculations, StringEqualCalculations}
 import domain.value.Value
 import domain.value.generator.{DateValueGenerator, NumberValueGenerator, StringValueGenerator, ValueGenerator}
 
 object InputDistributionConversions {
 
-  def inputFieldToValueGenerator(in: InputField[_]): ValueGenerator[_, _] =
-    InputFieldConversions.inputFieldToValueGenerator(in)
-
-  def valueGeneratorToDistribution(vg: ValueGenerator[_, _],
-    dist: List[InputDistribution]): Flow[Value[_], Value[_], NotUsed] = {
-
-    def eraseType[A, B, C](f: Flow[Value[A], Value[B], C]) = f.asInstanceOf[Flow[Value[_], Value[_], C]]
-
-    vg match {
-      case value: StringValueGenerator =>
-        eraseType(Flow.fromGraph(new DistributionFlowString(value, dist)))
-      case value: NumberValueGenerator =>
-        eraseType(Flow.fromGraph(new DistributionFlowNumber(value, dist)))
-      case value: DateValueGenerator =>
-        eraseType(Flow.fromGraph(new DistributionFlowDate(value, dist)))
-    }
-
-
+  def inputFieldToValueGenerator(in: InputField[_]): ValueGenerator[_, _] = in.getOptions match {
+    case _: OptionsString =>
+      new StringValueGenerator(
+        in,
+        new StringEqualCalculations(in.getOptions.asInstanceOf[OptionsString].getAcceptedStrings))
+    case _: OptionsNumber =>
+      new NumberValueGenerator(
+        in,
+        new NumberEqualCalculations(in.getOptions.asInstanceOf[OptionsNumber].getRanges)
+      )
+    case _: OptionsDate =>
+      new DateValueGenerator(
+        in,
+        new DateDistributionCalculations(
+          in.getOptions.asInstanceOf[OptionsDate].getStartingDate,
+          in.getOptions.asInstanceOf[OptionsDate].getTimeIncrement)
+      )
   }
 
 
+  def valueGeneratorToDistribution(vg: ValueGenerator[_, _],
+                                   dist: List[InputDistribution]): Flow[Value[_], Value[_], NotUsed] = vg match {
+    /*    case value : StringValueGenerator =>
+           Flow.fromGraph(new DistributionFlowString(value, dist))
+        case value: NumberValueGenerator =>
+          Flow.fromGraph(new DistributionFlowNumber(value, dist))*/
+    case value: DateValueGenerator =>
+      Flow.fromGraph(new DistributionFlowDate(value, dist)).asInstanceOf[Flow[Value[_], Value[_], NotUsed]]
+  }
 }
---
+++ src/main/scala/domain/stream/stage/conversion/InputDistributionConversionsInvalid.scala
@@ -0,0 +1,40 @@
+package domain.stream.stage.conversion
+
+
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+import domain.in.distribution.InputDistribution
+import domain.in.field.InputField
+import domain.stream.stage.flow.distribution.{DistributionFlowDate, DistributionFlowNumber, DistributionFlowString}
+
+import domain.value.Value
+import domain.value.generator.{DateValueGenerator, NumberValueGenerator, StringValueGenerator, ValueGenerator}
+
+
+object InputDistributionConversionsInvalid {
+
+
+  def inputFieldToValueGenerator(in: InputField[_]): ValueGenerator[_, _] =
+    InputFieldConversions.inputFieldToValueGenerator(in)
+
+
+  def valueGeneratorToDistribution(vg: ValueGenerator[_, _],
+    dist: List[InputDistribution]): Flow[Value[_], Value[_], NotUsed] = {
+
+    def eraseType[A, B, C](f: Flow[Value[A], Value[B], C]) = f.asInstanceOf[Flow[Value[_], Value[_], C]]
+
+    vg match {
+      case value: StringValueGenerator =>
+        eraseType(Flow.fromGraph(new DistributionFlowString(value, dist)))
+      case value: NumberValueGenerator =>
+        eraseType(Flow.fromGraph(new DistributionFlowNumber(value, dist)))
+      case value: DateValueGenerator =>
+        eraseType(Flow.fromGraph(new DistributionFlowDate(value, dist)))
+    }
+
+
+  }
+  
+
+
+}
