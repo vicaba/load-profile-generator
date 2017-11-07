0eabd6629e10bf912bc8b12e4f998ff0a8dbbbef
Albert <atriast@gmail.com>
2017-11-07 16:40:45 +0100

    Made MergeNodeTest the official MergeNode as it works now. Also changed GraphGenerator so it works with the new MergeNode.

--- src/main/scala/GraphGenerator.scala
+++ src/main/scala/GraphGenerator.scala
@@ -1,11 +1,8 @@
 import akka.stream.scaladsl.Broadcast
 import domain.in.config.ConfigHolder
-import domain.in.distribution.InputDistribution
-import domain.in.field.InputField
-import domain.in.field.options.Options
-import domain.stream.stage.conversion.{InputDistributionConversions, InputDistributionConversionsInvalid, InputFieldConversions}
+import domain.stream.stage.conversion.{InputDistributionConversions, InputFieldConversions}
 import domain.stream.stage.flow.rules.RulesFlow
-import domain.stream.stage.merge.{MergeNode, MergeNodeTest}
+import domain.stream.stage.merge.{MergeNode}
 import domain.transform.rule.RulesCheck
 import domain.value.Value
 
@@ -14,23 +11,6 @@ import scala.languageFeature.implicitConversions
 
 
 class GraphGenerator {
-  //implicit def sourceValueTToSource[V](st: SourceValueT[V, _]): Source[Value[V], NotUsed] = Source.fromGraph(st)
-
-  /**
-    * Java API
-    *
-    * @param listFields
-    * @param rulesCheck
-    * @param distributions
-    */
-  def generate(
-                listFields: java.util.ArrayList[InputField[Options]],
-                rulesCheck: RulesCheck,
-                distributions: java.util.ArrayList[InputDistribution]): Unit = {
-
-    generate(listFields.asScala.toList, rulesCheck, distributions.asScala.toList)
-
-  }
 
   def generate(configHolder: ConfigHolder,
                rulesCheck: RulesCheck): Unit = {
@@ -68,76 +48,9 @@ class GraphGenerator {
 
       val rulesFlow = new RulesFlow(rulesCheck)
 
-      val mergeNodeTest = new MergeNodeTest(mapSources, mapBroadcasts, mapDistributions, listConnections, rulesFlow)
+      val mergeNodeTest = new MergeNode(mapSources, mapBroadcasts, mapDistributions, listConnections, rulesFlow)
       mergeNodeTest.connectAndRunGraph()
     }
   }
 
-  /**
-    * Scala API
-    *
-    * @param inputFields
-    * @param rulesCheck
-    * @param distributions
-    */
-  def generate(
-                inputFields: List[InputField[Options]],
-                rulesCheck: RulesCheck,
-                distributions: List[InputDistribution]): Unit = {
-
-    if (inputFields.nonEmpty) {
-
-      val mapSources = inputFields
-        .map(InputFieldConversions.inputFieldToValueGenerator)
-        .map(vg => vg.getId -> InputFieldConversions.valueGeneratorToSource(vg))
-        .toMap
-
-      println(s"Elements in map1 = $mapSources")
-
-      val mapBroadcasts = distributions
-        .map(broad => broad.getId -> Broadcast[Value[_]](2))
-        .toMap
-
-      println(s"Elements in map2 = $mapBroadcasts")
-
-      val mapListDist = distributions
-        .map(mapDist => mapDist.getResult.getId -> mapDist)
-        .toMap
-
-      val mapDistributions = inputFields
-        .map(InputDistributionConversions.inputFieldToValueGenerator)
-        .map(mapDist => mapListDist(mapDist.getId).getResult.getId
-          -> InputDistributionConversions.valueGeneratorToDistribution(
-          mapDist, List(mapListDist(mapDist.getId))))
-        .toMap
-
-      //TODO Distribution nodes will have as key idSourceThatAffectsDistribution:idSourceToBeDistributed
-      //TODO We will always check if id exists in Broadcast, if it doesn't we will use Source.
-      //TODO First outlet of Broadcast (0) will always be the outlet that is not used for distribution
-      //TODO Second (1) will only be used for Distribution purposes, and nothing more.
-
-      val rulesFlow = new RulesFlow(rulesCheck)
-
-      val mergeRun = new MergeNode(mapSources, mapBroadcasts, mapDistributions, rulesFlow)
-      mergeRun.connectAndRunGraph()
-
-    }
-
-  }
-
-
-  //TODO The combine method merges all sources into one.
-  //TODO If for example we have 3 columns, it will generate an entire data each 3 iterations (test below generates 10 data of 3 columns).
-  //TODO The plan would be to grab the 3 columns, put them together in one single List, and pass them to a Sink
-  //TODO This sink will do whatever job we need it to do with the merged value.
-  //TODO Now to figure out how to do it with any number of columns...
-  /*  val mergedResult: Future[Done] = merged.take(30).runWith(
-      Sink.foreach(i => println(i.getId + " " + i.getType + " " + i.getValue.toString))
-    )*/
-
-  /*
-  Future.sequence(Seq(doneNumber, doneString, doneDate)).andThen {
-    case _ => system.terminate()
-  }
-  */
 }
--- src/main/scala/domain/stream/stage/merge/MergeNode.scala
+++ src/main/scala/domain/stream/stage/merge/MergeNode.scala
@@ -2,53 +2,77 @@ package domain.stream.stage.merge
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, RunnableGraph, Sink, Source, ZipN}
+import akka.stream.scaladsl.{Broadcast, Flow, GraphDSL, Merge, RunnableGraph, Sink, Source, ZipN}
 import akka.stream.{ActorMaterializer, ClosedShape}
+import domain.in.distribution.InputDistribution
 import domain.stream.stage.flow.rules.RulesFlow
 import domain.value.Value
 
 import scala.concurrent.ExecutionContextExecutor
 
-class MergeNode(values: Map[String, Source[Value[_], NotUsed]],
+class MergeNode(sourceValues: Map[String, Source[Value[_], NotUsed]],
                 broadcastValues: Map[String, Broadcast[Value[_]]],
-                distributionValues: Map[String, Flow[Value[_],Value[_], NotUsed]],
+                distributionValues: Map[String, Flow[Value[_], Value[_], NotUsed]],
+                listConnections: Map[String, List[InputDistribution]],
                 rulesNode: RulesFlow) {
   implicit val system2: ActorSystem = ActorSystem("QuickStart")
   implicit val materializer2: ActorMaterializer = ActorMaterializer()
   implicit val ec2: ExecutionContextExecutor = system2.dispatcher
-
-  val listSources: Map[String, Source[Value[_], NotUsed]] = values
-  val rulesFlow: Flow[Seq[Value[_]], Seq[Value[_]], NotUsed] = Flow.fromGraph(rulesNode)
-  val broadcastFlows: Map[String, Broadcast[Value[_]]] = broadcastValues
 
   def connectAndRunGraph(): NotUsed = {
 
     RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
       import GraphDSL.Implicits._
 
-      val zipper = builder.add(ZipN[Value[_]](values.size))
+      val zipper = builder.add(ZipN[Value[_]](sourceValues.size + distributionValues.size))
 
-      values.keys.foreach(src =>
-        if (broadcastFlows.contains(src)) {
-          println("Broadcast, ID is "+src)
-          val broadcast = builder.add(broadcastFlows(src))
-          values(src) ~> broadcast.in
-          broadcast.out(0) ~> zipper
-          //TODO Connect Broadcast to Distribution, and distribution to zipper
-          broadcast.out(1) ~> distributionValues(src) ~> zipper //TO BE CHANGED
-        } else {
-          println("No Broadcast, ID is "+src)
-          values(src) ~> zipper
+
+      println("1. Time to connect sources to zipper")
+      sourceValues foreach (src =>
+        if (!broadcastValues.contains(src._1)) {
+          println("---Connecting Source " + src._1.toString + " with zipper")
+          src._2 ~> zipper
+          println("---Connected")
         }
-      )
+        )
 
-      zipper ~> rulesFlow ~> Flow[Seq[Value[_]]].map(s => s.foreach(
+      val broadBuild = broadcastValues
+        .map(broad => broad._1 -> builder.add(broad._2))
+
+
+      println("2. Time to connect sources with broadcast. Broad size is " + broadcastValues.size)
+      for ((id, broad) <- broadBuild) {
+        println("---Connecting " + sourceValues(id).toString() + " with " + broad.in.toString())
+        sourceValues(id) ~> broad.in
+        println("---Connecting " + broad.out(0).toString() + " with zipper")
+        broad.out(0) ~> zipper
+        println("---Connected")
+      }
+
+      println("3. Time to connect Broadcast with Distribution")
+      distributionValues foreach { flow =>
+        val conn = listConnections(flow._1)
+        if (conn.size == 1) {
+          println("---Connecting " + broadBuild(conn.head.getId).out(1) + " with " + flow._2)
+          broadBuild(conn.head.getId).out(1) ~> flow._2 ~> zipper
+          println("---Connected")
+        } else {
+          val merge = builder.add(Merge[Value[_]](conn.size))
+          conn.foreach { conn =>
+            println("---Connecting " + broadBuild(conn.getId).out(1) + " with " + flow._2)
+            broadBuild(conn.getId).out(1) ~> merge
+            println("---Connected")
+          }
+          merge ~> flow._2 ~> zipper
+        }
+      }
+
+      zipper ~> rulesNode ~> Flow[Seq[Value[_]]].map(s => s.foreach(
         src => println(src.getId + "||" + src.getType + "||" + src.getValue)
       )) ~> Sink.ignore
 
-
       ClosedShape
-    }).run()
+    }).run
 
   }
 
