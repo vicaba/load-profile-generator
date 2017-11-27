a4aecc2e97a0bf85ac9be3060aeed54e2d7e3b08
Albert <atriast@gmail.com>
2017-11-27 17:29:14 +0100

    Changed CreateTemplate so it uses constants for the initial configuration (still need to add more). Also changed CreateTemplate so in case it receives a non-existing type, it defaults to one that we can decide (currently json), so the program doesn't stop if it doesn't exist.

--- src/main/java/domain/out/template/CreateTemplate.java
+++ src/main/java/domain/out/template/CreateTemplate.java
@@ -1,26 +1,30 @@
 package domain.out.template;
 
 import domain.value.Value;
-import freemarker.template.Configuration;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
-import freemarker.template.TemplateExceptionHandler;
+import freemarker.template.*;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.*;
 
 public class CreateTemplate {
+  private static final Version CONFIGURATION_VERSION = Configuration.VERSION_2_3_26;
+  private static final String TEMPLATES_FOLDER = "templates";
+  private static final String DEFAULT_ENCODING = "UTF-8";
+  private static final List<String> ACCEPTED_TYPES = Arrays.asList("json", "html", "csv");
+  private static final String DEFAULT_ACCEPTED_TYPE = "json";
 
   private Configuration cfg;
   private ArrayList<List<Value>> outputs;
-  private static final List<String> ACCEPTED_TYPES = Arrays.asList("html", "json", "csv");
-  private String type = "";
+  private String type = DEFAULT_ACCEPTED_TYPE;
 
   public CreateTemplate(String type) {
     try {
-      this.cfg = new Configuration(Configuration.VERSION_2_3_26);
-      this.cfg.setDirectoryForTemplateLoading(new File("templates"));
-      this.cfg.setDefaultEncoding("UTF-8");
+      this.cfg = new Configuration(CONFIGURATION_VERSION);
+      this.cfg.setDirectoryForTemplateLoading(new File(TEMPLATES_FOLDER));
+      this.cfg.setDefaultEncoding(DEFAULT_ENCODING);
       this.cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
       this.cfg.setLogTemplateExceptions(false);
       this.outputs = new ArrayList<>();
@@ -37,23 +41,28 @@ public class CreateTemplate {
   public void createObjectTemplate() {
     try {
       Map<String, Object> root = new HashMap<>();
-      //root.put("headers", outputs.get(0));
+      // root.put("headers", outputs.get(0));
 
       root.put("outputs", this.outputs);
 
-      //TODO SET A DEFAULT BEHAVIOR
-      if (ACCEPTED_TYPES.contains(this.type)) {
-        Template template = this.cfg.getTemplate("template_" + this.type + ".ftlh");
-        // For output in console, use this one. Testing only.
-        //Writer out = new OutputStreamWriter(System.out);
-
-        // For output in file, use this one.
-        Writer out = new FileWriter(new File("output/data." + type));
-
-        template.process(root, out);
-        out.flush();
-        out.close();
+      /*
+       * If value doesn't exist inside the accepted types,
+       * it will use the default value specified instead so program is not interrupted by this error.
+       */
+      //TODO Create a log informing the user that the type added doesn't exist and that it has defaulted to another.
+      if (!ACCEPTED_TYPES.contains(this.type)) {
+        this.type = DEFAULT_ACCEPTED_TYPE;
       }
+      Template template = this.cfg.getTemplate("template_" + this.type + ".ftlh");
+      // For output in console, use this one. Testing only.
+      // Writer out = new OutputStreamWriter(System.out);
+
+      // For output in file, use this one.
+      Writer out = new FileWriter(new File("output/data." + type));
+
+      template.process(root, out);
+      out.flush();
+      out.close();
     } catch (IOException | TemplateException e) {
       e.printStackTrace();
     }
