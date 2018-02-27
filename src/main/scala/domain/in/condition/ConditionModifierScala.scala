40855d2782971f2e8bd050b765df247dfcd212cf
Albert <atriast@gmail.com>
2018-02-27 14:12:50 +0100

    Added ConditionModifier scala version

---
+++ src/main/scala/domain/in/condition/ConditionModifierScala.scala
@@ -0,0 +1,7 @@
+package domain.in.condition
+
+final class ConditionModifierScala[T](id: String, operation: String, value: T) {
+  def getId: String = this.id
+  def getOperation: String = this.operation
+  def getValue: T = this.value
+}
