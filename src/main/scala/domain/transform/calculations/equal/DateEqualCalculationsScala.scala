729e7414019b0e8e7a8be146a603996a5011f934
Albert <atriast@gmail.com>
2018-03-05 15:42:39 +0100

    Made a small change to the name of a value inside DateEqualCalculations.

--- src/main/scala/domain/transform/calculations/equal/DateEqualCalculationsScala.scala
+++ src/main/scala/domain/transform/calculations/equal/DateEqualCalculationsScala.scala
@@ -6,10 +6,10 @@ import java.time.format.DateTimeFormatter
 final class DateEqualCalculationsScala(startingDate:String, timeIncrement: Int) extends EqualCalculationsScala[LocalDateTime] {
   private final val DateFormat = "dd-MM-yyyy HH:mm"
 
-  private var CurrentDate = LocalDateTime.parse(startingDate, DateTimeFormatter.ofPattern(this.DateFormat))
+  private var currentDate = LocalDateTime.parse(startingDate, DateTimeFormatter.ofPattern(this.DateFormat))
 
   override def calculate: LocalDateTime = {
-    this.CurrentDate = this.CurrentDate.plusSeconds(this.timeIncrement)
-    this.CurrentDate
+    this.currentDate = this.currentDate.plusSeconds(this.timeIncrement)
+    this.currentDate
   }
 }
