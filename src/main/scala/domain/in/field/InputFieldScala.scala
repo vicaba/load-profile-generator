4d73e7ec458a8ca76b3915e27b53a38c17534bd5
Albert <atriast@gmail.com>
2018-03-05 14:03:56 +0100

    Updated InfoFieldScala so it also gives name and type too.

--- src/main/scala/domain/in/field/InputFieldScala.scala
+++ src/main/scala/domain/in/field/InputFieldScala.scala
@@ -3,6 +3,10 @@ package domain.in.field
 import domain.in.field.options.OptionsScala
 
 final class InputFieldScala[T <: OptionsScala](info: InputFieldInfoScala, options: T, distributionInfo: DistributionInfoScala) {
+  def getName: String = this.info.getName
+
+  def getType: String = this.info.getType
+
   def getId: String = this.info.getId
 
   def getInfo: InputFieldInfoScala = this.info
