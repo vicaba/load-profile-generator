e9820a1cca7c46057ed1a619c8606a1557ae7388
Albert <atriast@gmail.com>
2018-03-05 13:53:22 +0100

    Added getId inside InputFieldScala due to GraphGenerator requirements.

--- src/main/scala/domain/in/field/InputFieldScala.scala
+++ src/main/scala/domain/in/field/InputFieldScala.scala
@@ -3,6 +3,8 @@ package domain.in.field
 import domain.in.field.options.OptionsScala
 
 final class InputFieldScala[T <: OptionsScala](info: InputFieldInfoScala, options: T, distributionInfo: DistributionInfoScala) {
+  def getId: String = this.info.getId
+
   def getInfo: InputFieldInfoScala = this.info
 
   def getOptions: T = this.options
