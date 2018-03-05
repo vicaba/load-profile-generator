48b57b84bf914c70a3b7001f899fa2da72f0fed2
Albert <atriast@gmail.com>
2018-03-05 18:19:05 +0100

    Changed Generic Type of ConditionModifier in InputRule as it caused problems with the previous type specified.

--- src/main/scala/domain/in/rule/InputRuleScala.scala
+++ src/main/scala/domain/in/rule/InputRuleScala.scala
@@ -5,7 +5,7 @@ import domain.in.condition.ConditionModifierScala
 final class InputRuleScala[T](private val id: String,
                                    private val condition: String,
                                    private val comparator: T,
-                                   private val result: ConditionModifierScala[T]) {
+                                   private val result: ConditionModifierScala[_]) {
 
   def getId: String = this.id
 
@@ -13,5 +13,5 @@ final class InputRuleScala[T](private val id: String,
 
   def getComparator: T = this.comparator
 
-  def getResult: ConditionModifierScala[T] = this.result
+  def getResult: ConditionModifierScala[_] = this.result
 }
