494c570f17f155ea7ba9d03fed5d61a0c6a33b11
Albert <atriast@gmail.com>
2018-03-05 13:08:25 +0100

    Created scala version of OutputConfigurationJsonReader.

---
+++ src/main/scala/infrastructure/out/config/serialization/json/deserializer/OutputConfigurationJsonReaderScala.scala
@@ -0,0 +1,19 @@
+package infrastructure.out.config.serialization.json.deserializer
+
+import domain.out.field.OutputFieldScala
+import org.json4s.jackson.JsonMethods._
+import org.json4s.{DefaultFormats, Formats, _}
+
+import scala.io.Source
+
+final class OutputConfigurationJsonReaderScala(path: String) {
+  implicit private val formats: Formats = DefaultFormats
+
+  def readJson4(): Unit = {
+    val json = parse(Source.fromFile(path).mkString)
+    print(json + "\n")
+    val outputField = json.extract[OutputFieldScala]
+    print(outputField.getNameTemplate + " - " + outputField.getOutputType)
+
+  }
+}
