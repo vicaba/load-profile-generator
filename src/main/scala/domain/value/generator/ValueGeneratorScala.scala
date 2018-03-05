f7471f051539bdfc921e797c7b183f61dd81c6a2
Albert <atriast@gmail.com>
2018-03-05 14:19:53 +0100

    Created initial scala version of ValueGenerator, lacks the scala version of DistributionsCalculations.

---
+++ src/main/scala/domain/value/generator/ValueGeneratorScala.scala
@@ -0,0 +1,31 @@
+package domain.value.generator
+
+import domain.in.field.InputFieldScala
+import domain.transform.calculations.CalculationsScala
+import domain.value.ValueScala
+
+abstract class ValueGeneratorScala[T, V <: CalculationsScala[T]](inputField: InputFieldScala[T], calculations: V) {
+  def obtainNext: ValueScala[T] = new ValueScala[T](
+    this.inputField.getId,
+    this.inputField.getType,
+    this.inputField.getName,
+    this.calculations.calculate
+  )
+
+  def getId: String = inputField.getId
+
+  def getName: String = inputField.getName
+
+  def increaseCounter(): Unit = {
+    /*
+    this.calculations match {
+      case value: DistributionCalculationsScala[T] =>
+        value.increaseDistributionCounter()
+      case _ =>
+    }
+    */
+  }
+}
+
+
+
