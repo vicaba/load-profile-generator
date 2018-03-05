0c5954a2cfa259f4e7e20539300f1b863220cb3b
Albert <atriast@gmail.com>
2018-03-05 13:07:57 +0100

    Created scala version of OutputField.

---
+++ src/main/scala/domain/out/field/OutputFieldScala.scala
@@ -0,0 +1,11 @@
+package domain.out.field
+
+final class OutputFieldScala(nameTemplate: String, outputType: String) {
+  def getNameTemplate: String = {
+    this.nameTemplate
+  }
+
+  def getOutputType: String = {
+    this.outputType
+  }
+}
