1c2cd27647d9d6cb2a88b527a34fb92b41d05734
Albert <atriast@gmail.com>
2018-03-05 13:40:03 +0100

    Added Logger in MainTest

--- src/main/scala/MainTest.scala
+++ src/main/scala/MainTest.scala
@@ -1,11 +1,52 @@
+import java.lang.System.exit
+
+import Main.{DefaultLoggerConfigurationPath, configureLogger}
+import ch.qos.logback.classic.LoggerContext
+import ch.qos.logback.classic.joran.JoranConfigurator
+import ch.qos.logback.core.joran.spi.JoranException
+import ch.qos.logback.core.util.StatusPrinter
 import domain.graph.GraphGenerator
 import domain.out.template.{FreemakerTemplateSystemScala, TemplateOutputScala}
 import domain.transform.rule.RulesCheckScala
 import infrastructure.in.config.json.deserializer.InputConfigurationJsonReaderScala
 import infrastructure.out.config.serialization.json.deserializer.OutputConfigurationJsonReaderScala
+import org.slf4j.{Logger, LoggerFactory}
 
 object MainTest {
+
+  private final val DefaultLoggerConfigurationPath = "resources/logback.xml"
+
+  private def configureLogger(loggerConfigurationFilePath: String): Unit = {
+    val context: LoggerContext = LoggerFactory.getILoggerFactory.asInstanceOf[LoggerContext]
+    try {
+      val configurator: JoranConfigurator = new JoranConfigurator
+      configurator.setContext(context)
+      // Call context.reset() to clear any previous configuration, e.g. default
+      // configuration. For multi-step configuration, omit calling context.reset().
+      context.reset()
+      configurator.doConfigure(loggerConfigurationFilePath)
+    } catch {
+      case je: JoranException =>
+        je.printStackTrace()
+    }
+    StatusPrinter.printInCaseOfErrorsOrWarnings(context)
+  }
+
   def main(args: Array[String]): Unit = {
+
+    configureLogger(DefaultLoggerConfigurationPath)
+
+    val appLogger: Logger = LoggerFactory.getLogger("app.logger")
+
+    if (args.length != 2) {
+      val usageMessage = "./application <path/to/input/configuration/file> <path/to/output/configuration/file>"
+      appLogger.info(usageMessage)
+      println(usageMessage)
+      exit(0)
+    }
+
+    appLogger.info("Starting Application")
+
     val inputConfigurationFile = "resources/input.json"
     val inputConfiguration = new InputConfigurationJsonReaderScala(inputConfigurationFile).readJson4()
     val outputConfigurationFile = "resources/output.json"
