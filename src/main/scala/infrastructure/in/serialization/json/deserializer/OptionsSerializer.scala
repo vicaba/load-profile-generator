c9af8e9fa0f2d0e9a097619dfc95bc94829433ea
Albert <atriast@gmail.com>
2018-03-07 16:11:31 +0100

    Updated OptionsSerializer so it now treats the new OptionAppliance.

--- src/main/scala/infrastructure/in/serialization/json/deserializer/OptionsSerializer.scala
+++ src/main/scala/infrastructure/in/serialization/json/deserializer/OptionsSerializer.scala
@@ -1,11 +1,11 @@
 package infrastructure.in.serialization.json.deserializer
 
-import domain.in.field.options.{OptionsScala, OptionsScalaDate, OptionsScalaNumber, OptionsScalaString}
+import domain.in.field.options._
 import domain.in.field.{DistributionInfoScala, InputFieldInfoScala, InputFieldScala}
 import org.json4s.jackson.JsonMethods.parse
 import org.json4s.{CustomSerializer, DefaultFormats, Formats, JObject, MappingException}
 
-object OptionsSerializer extends CustomSerializer[InputFieldScala[OptionsScala]](_ => ( {
+object OptionsSerializer extends CustomSerializer[InputFieldScala](_ => ( {
 
   case x: JObject =>
     implicit val formats: Formats = DefaultFormats
@@ -14,21 +14,25 @@ object OptionsSerializer extends CustomSerializer[InputFieldScala[OptionsScala]]
     val info = (x \ "info").extract[InputFieldInfoScala]
     info.getType match {
       case "string" =>
-        print("Found a string\n")
         val options = (x \ "options").extract[OptionsScalaString]
         new InputFieldScala(info, options, distributionInfo.orNull)
+
       case "date" =>
-        print("Found a date\n")
         val options = (x \ "options").extract[OptionsScalaDate]
         new InputFieldScala(info, options, distributionInfo.orNull)
+
       case "integer" | "decimal" =>
-        print("Found a number\n")
         val options = (x \ "options").extract[OptionsScalaNumber]
         new InputFieldScala(info, options, distributionInfo.orNull)
-      case null => throw new MappingException("Can't convert bar with name " + x + " to Bar")
+
+      case "appliance" =>
+        val options = (x \ "options").extract[OptionsScalaAppliance]
+        new InputFieldScala(info, options, distributionInfo.orNull)
+
+      case null => throw new MappingException("Can't convert options with name " + x + " to OptionsScala")
     }
 }, {
-  case x: InputFieldScala[OptionsScala] =>
+  case x: InputFieldScala =>
     parse("{test: 0}")
 }
 ))
